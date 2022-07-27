2040f0f738fbf19bad2a4d44de20b9e0042b4dea
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-07-28 01:53:10 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -17,14 +17,22 @@ halo = as.numeric(args[2])
 
 df = read.csv(inputfile, header = T)
 
-plot.df = df[df$halo == halo,]
+plot.df.1 = df[df$halo == halo & df$p == 0 & df$q == 0, ]
+plot.df.2 = df[df$halo == halo & df$p == 1 & df$q == 1, ]
+plot.df.3 = df[df$halo == halo & df$p == 1 & df$q == 0, ]
 
-p1.1 = ggplot(data = plot.df)+
+p1.1 = ggplot(data = plot.df.1)+
+  geom_point(aes(x = rho, y = mh, col = as.factor(t)))+
+  facet_wrap(~nseed)
+p1.2 = ggplot(data = plot.df.2)+
+  geom_point(aes(x = rho, y = mh, col = as.factor(t)))+
+  facet_wrap(~nseed)
+p1.3 = ggplot(data = plot.df.3)+
   geom_point(aes(x = rho, y = mh, col = as.factor(t)))+
   facet_wrap(~nseed)
   
   filename = paste("test-",ifelse(halo>0,yes = "repel-",""),".png",sep = "")
   res.factor = 3
   png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
-  p1.1
+  grid.arrange(p1.1,p1.2,p1.3,nrow = 3)
   dev.off()
