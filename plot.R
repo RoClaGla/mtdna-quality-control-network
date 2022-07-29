292f37ec55fe5d049d2682ff0a8ccb220720f58d
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-07-30 00:08:12 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -17,38 +17,12 @@ halo = as.numeric(args[2])
 
 df = read.csv(inputfile, header = T)
 
-plot.df.1 = df[df$halo == halo & df$p == 0 & df$q == 0, ]
-plot.df.2 = df[df$halo == halo & df$p == 1 & df$q == 1, ]
-plot.df.3 = df[df$halo == halo & df$p == 1 & df$q == 0, ]
-
-p1.1 = ggplot(data = plot.df.1)+
-  geom_line(aes(x = rho, y = mh, col = as.factor(t)))+
-  facet_wrap(~nseed)
-p1.2 = ggplot(data = plot.df.2)+
-  geom_line(aes(x = rho, y = mh, col = as.factor(t)))+
-  facet_wrap(~nseed)
-p1.3 = ggplot(data = plot.df.3)+
-  geom_line(aes(x = rho, y = mh, col = as.factor(t)))+
-  facet_wrap(~nseed)
+p1.1 = ggplot(data = plot.df)+
+  geom_line(aes(x = p, y = q, col = as.factor(t)))+
+  facet_wrap(rho~nseed)
   
-  filename = paste("mh-",ifelse(halo>0,yes = "repel-",""),".png",sep = "")
-  res.factor = 3
-  png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
-  grid.arrange(p1.1,p1.2,p1.3,nrow = 3)
-  dev.off()
-
-p2.1 = ggplot(data = plot.df.1)+
-  geom_line(aes(x = rho, y = vh, col = as.factor(t)))+
-  facet_wrap(~nseed)
-p2.2 = ggplot(data = plot.df.2)+
-  geom_line(aes(x = rho, y = vh, col = as.factor(t)))+
-  facet_wrap(~nseed)
-p2.3 = ggplot(data = plot.df.3)+
-  geom_line(aes(x = rho, y = vh, col = as.factor(t)))+
-  facet_wrap(~nseed)
-  
-  filename = paste("vh-",ifelse(halo>0,yes = "repel-",""),".png",sep = "")
-  res.factor = 3
-  png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
-  grid.arrange(p2.1,p2.2,p2.3,nrow = 3)
-  dev.off()
+filename = paste("mh-",ifelse(halo>0,yes = "repel-",""),".png",sep = "")
+res.factor = 3
+png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
+grid.arrange(p1.1,p1.2,p1.3,nrow = 3)
+dev.off()
