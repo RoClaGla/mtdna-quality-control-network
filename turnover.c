16416baa989a874d69f73d1679b3cb31ac6f80ba
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-05 10:43:26 +0200

    Update turnover.c

--- turnover.c
+++ turnover.c
@@ -701,7 +701,7 @@ int main(int argc, char *argv[]){
       for(p=0.0;p<=1.0;p+=0.1){
         for(q=0.0;q<=1.0;q+=0.1){
           for(halo=0;halo<=0.1;halo+=0.1){
-            for(rho=0.01;rho<=0.25;rho+=0.05){
+            for(rho=0.05;rho<=0.25;rho+=0.1){
 							K=0;
 							while(K<25){
 								for(mut_rate=0.0;mut_rate<0.10;mut_rate+=0.025){
