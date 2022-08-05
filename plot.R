6fa9b3271254007b4553c7ca76dbeefacb6a4e13
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-05 21:44:50 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -23,7 +23,6 @@ TO_RATE = unique(df$to_rate)
 
 plot.df.1 = df[df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[1],]
 plot.df.2 = df[df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[2],]
-plot.df.3 = df[df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[3],]
 
 vhmax = max(max(plot.df.1$vh),max(plot.df.2$vh),max(plot.df.3$vh))
 bl = h*(1-h)/n
@@ -35,12 +34,9 @@ p1.1 = ggplot(data = plot.df.1)+#fn+
 p1.2 = ggplot(data = plot.df.2)+#fn+
   geom_tile(aes(x = p, y = q, fill = vh))+
   facet_wrap(~nseed)
-p1.3 = ggplot(data = plot.df.3)+#fn+
-  geom_tile(aes(x = p, y = q, fill = vh))+
-  facet_wrap(~nseed)
   
 filename = paste("vh",ifelse(halo>0,yes = "-repel",""),".png",sep = "")
 res.factor = 3
 png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
-grid.arrange(p1.1,p1.2,p1.3,nrow = 3)
+grid.arrange(p1.1,p1.2,nrow = 3)
 dev.off()
