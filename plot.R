3e4bcd1a4826c8278112aad141a9ec3a57dfb03c
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-08 08:13:52 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -15,9 +15,10 @@ if(length(args)<4){
 inputfile = args[1]
 n = as.numeric(args[2])
 h = as.numeric(args[3])
-halo = as.numeric(args[4])
-mut_rate = as.numeric(args[5])
-to_rate = as.numeric(args[6])
+nseed = as.numeric(args[4])
+halo = as.numeric(args[5])
+mut_rate = as.numeric(args[6])
+to_rate = as.numeric(args[7])
 
 
 # Function to predict V(h) from E(h^2)-E(h)^2 with statistical simulations
@@ -80,11 +81,11 @@ df = read.csv(inputfile, header = T)
 MUT_RATE = unique(df$mut_rate)
 TO_RATE = unique(df$to_rate)
 
-plot.df.1 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[1] & df$K == 0,]
-plot.df.2 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[1] & df$K == 5,]
-plot.df.3 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[1] & df$K == 10,]
-plot.df.4 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[1] & df$K == 15,]
-plot.df.5 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == MUT_RATE[1] & df$to_rate == TO_RATE[1] & df$K == 20,]
+plot.df.1 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == mut_rate & df$to_rate == to_rate & df$K == 0,]
+plot.df.2 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == mut_rate & df$to_rate == to_rate & df$K == 5,]
+plot.df.3 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == mut_rate & df$to_rate == to_rate & df$K == 10,]
+plot.df.4 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == mut_rate & df$to_rate == to_rate & df$K == 15,]
+plot.df.5 = df[df$halo == halo & df$t == 0 & df$rho == 0.05 & df$mut_rate == mut_rate & df$to_rate == to_rate & df$K == 20,]
 
 plot.df.1$vhprime = plot.df.1$vh/(plot.df.1$mh*(1-plot.df.1$mh))
 plot.df.2$vhprime = plot.df.2$vh/(plot.df.2$mh*(1-plot.df.2$mh))
@@ -115,26 +116,19 @@ p5.1 = ggplot(data = plot.df.5)+
   geom_tile(aes(x = p, y = q, fill = vhprime))+colfn+
   facet_wrap(~nseed)
 
-filename = paste("vh",ifelse(halo>0,yes = "-repel",""),".png",sep = "")
+filename = paste("vh-",toString(mut_rate),"-",toString(to_rate),ifelse(halo>0,yes = "-repel",""),".png",sep = "")
 res.factor = 3
 png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
 grid.arrange(p1.1,p2.1,p3.1,p4.1,p5.1,nrow = 5)
 dev.off()
 
 
-plot.df.1 = df[df$halo == halo & df$nseed == 4 & df$mut_rate == MUT_RATE[1] & df$p == 1 & df$q == 0,]
-plot.df.2 = df[df$halo == halo & df$nseed == 4 & df$mut_rate == MUT_RATE[2] & df$p == 1 & df$q == 0,]
-plot.df.3 = df[df$halo == halo & df$nseed == 16 & df$mut_rate == MUT_RATE[1] & df$p == 1 & df$q == 0,]
-plot.df.4 = df[df$halo == halo & df$nseed == 16 & df$mut_rate == MUT_RATE[2] & df$p == 1 & df$q == 0,]
-plot.df.5 = df[df$halo == halo & df$nseed == 64 & df$mut_rate == MUT_RATE[1] & df$p == 1 & df$q == 0,]
-plot.df.6 = df[df$halo == halo & df$nseed == 64 & df$mut_rate == MUT_RATE[2] & df$p == 1 & df$q == 0,]
+plot.df.1 = df[df$halo == halo & df$nseed == nseed & df$mut_rate == mut_rate & df$p == 1 & df$q == 0,]
+plot.df.2 = df[df$halo == halo & df$nseed == nseed & df$mut_rate == mut_rate & df$p == 1 & df$q == 0,]
+
 
 plot.df.1$vhprime = plot.df.1$vh/(plot.df.1$mh*(1-plot.df.1$mh))
 plot.df.2$vhprime = plot.df.2$vh/(plot.df.2$mh*(1-plot.df.2$mh))
-plot.df.3$vhprime = plot.df.3$vh/(plot.df.3$mh*(1-plot.df.3$mh))
-plot.df.4$vhprime = plot.df.4$vh/(plot.df.4$mh*(1-plot.df.4$mh))
-plot.df.5$vhprime = plot.df.5$vh/(plot.df.5$mh*(1-plot.df.5$mh))
-plot.df.6$vhprime = plot.df.6$vh/(plot.df.6$mh*(1-plot.df.6$mh))
 
 p1.1 = ggplot(data = plot.df.1)+
   geom_line(aes(x = t, y = vhprime, col = as.factor(rho)))+
@@ -142,23 +136,11 @@ p1.1 = ggplot(data = plot.df.1)+
 p2.1 = ggplot(data = plot.df.2)+
   geom_line(aes(x = t, y = vhprime, col = as.factor(rho)))+
   facet_wrap(K~to_rate)
-p3.1 = ggplot(data = plot.df.3)+
-  geom_line(aes(x = t, y = vhprime, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
-p4.1 = ggplot(data = plot.df.4)+
-  geom_line(aes(x = t, y = vhprime, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
-p5.1 = ggplot(data = plot.df.5)+
-  geom_line(aes(x = t, y = vhprime, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
-p6.1 = ggplot(data = plot.df.6)+
-  geom_line(aes(x = t, y = vhprime, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
 
-filename = paste("vhprime-vs-turnover",ifelse(halo>0,yes = "-repel",""),".png",sep = "")
+filename = paste("vhprime-vs-turnover-",toString(nseed),"-"toString(mut_rate),"-",toString(to_rate),ifelse(halo>0,yes = "-repel",""),".png",sep = "")
 res.factor = 3
 png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
-grid.arrange(p1.1,p2.1,p3.1,p4.1,p5.1,p6.1,nrow = 6)
+grid.arrange(p1.1,p2.1,nrow = 2)
 dev.off()
 
 p1.1 = ggplot(data = plot.df.1)+
@@ -167,21 +149,9 @@ p1.1 = ggplot(data = plot.df.1)+
 p2.1 = ggplot(data = plot.df.2)+
   geom_line(aes(x = t, y = mh, col = as.factor(rho)))+
   facet_wrap(K~to_rate)
-p3.1 = ggplot(data = plot.df.3)+
-  geom_line(aes(x = t, y = mh, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
-p4.1 = ggplot(data = plot.df.4)+
-  geom_line(aes(x = t, y = mh, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
-p5.1 = ggplot(data = plot.df.5)+
-  geom_line(aes(x = t, y = mh, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
-p6.1 = ggplot(data = plot.df.6)+
-  geom_line(aes(x = t, y = mh, col = as.factor(rho)))+
-  facet_wrap(K~to_rate)
 
-filename = paste("mh-vs-turnover",ifelse(halo>0,yes = "-repel",""),".png",sep = "")
+filename = paste("mh-vs-turnover-",toString(nseed),"-",toString(mut_rate),"-",toString(to_rate),ifelse(halo>0,yes = "-repel",""),".png",sep = "")
 res.factor = 3
 png(filename, height = 1200*res.factor, width = 1200*res.factor, res = 72*res.factor)
-grid.arrange(p1.1,p2.1,p3.1,p4.1,p5.1,p6.1,nrow = 6)
+grid.arrange(p1.1,p2.1,nrow = 2)
 dev.off()
