731b3b1973af6bc79da904896e614eb308bcf29d
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-09 11:33:25 +0200

    Update turnover.c

--- turnover.c
+++ turnover.c
@@ -759,7 +759,7 @@ int main(int argc, char *argv[]){
 									correlateDNA(mx,my,mt,n,K);
 									// get DNA stats
 									getStats(mx,my,mt,mnetworked,n,&wc,&mc,&wn,&mn,&het);
-									getMutantProp(rho,mx,my,mt,n,&mutprop);
+									getMutantProp(rho,mx,my,mt,n,&mprop);
 									// fix so that all functions below are called, pass Stats directly to getStats
 									getSeparateProximalDNA(rho, mx, my, mt, mnetworked, n, &mproxnet, &mproxcyt);								
 									getNetworkProp(xs,ys,xe,ye,nsegs,&u);
