834630817ead1286f96fc66c98ec38b71168ad40
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-10 00:05:40 +0200

    Update analyse.sh

--- analyse.sh
+++ analyse.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
 
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
-
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0 
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0
-
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
-
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
-
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
-
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
-
+Rscript plot.R output-100-0.50.csv 100
