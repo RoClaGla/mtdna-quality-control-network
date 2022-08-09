fc4558204bf30a531e33a1d1e148efb0e2c48b69
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-09 22:58:17 +0200

    Update analyse.sh

--- analyse.sh
+++ analyse.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0 0.0 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0 0.0 0.025
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0 0.025 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0 0.025 0.025
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.0
 
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0 0.0 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0 0.0 0.025
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0 0.025 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0 0.025 0.025
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0 
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.0
 
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0 0.0 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0 0.0 0.025
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0 0.025 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0 0.025 0.025
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.0
 
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1 0.0 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1 0.0 0.025
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1 0.025 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1 0.025 0.025
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 4 0.1
 
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1 0.0 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1 0.0 0.025
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1 0.025 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1 0.025 0.025
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 16 0.1
 
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1 0.0 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1 0.0 0.025
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1 0.025 0.0
-Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1 0.025 0.025
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
+Rscript plot.R output-100-0.50.csv 100 0.5 64 0.1
 
