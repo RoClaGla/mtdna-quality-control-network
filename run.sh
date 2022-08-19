4cdef001ed56ec54cbac2377e33de293c40ed5f5
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-19 23:31:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,14 @@ gcc -o3 turnover.c -lm -o turnover.ce
 ./turnover.ce --snapshots 1 50 0.01 0.02 1.05 100 0.5 4 0.0 1.0 1.0 0.25 25 > tmps1 &  # 100-4-50-1.00-1.00-0.00-0.25
 ./turnover.ce --snapshots 1 50 0.01 0.02 1.05 100 0.5 16 0.0 1.0 1.0 0.25 25 > tmps2 & # 100-16-50-1.00-1.00-0.00-0.25
 ./turnover.ce --snapshots 1 50 0.01 0.02 1.05 100 0.5 64 0.0 1.0 1.0 0.0 25 > tmps3 &
-
 ./turnover.ce --snapshots 1 50 0.01 0.02 1.05 100 0.5 4 0.1 0.5 0.5 0.25 25 > tmps4 &
 ./turnover.ce --snapshots 1 50 0.01 0.02 1.05 100 0.5 16 0.1 0.5 0.5 0.25 25 > tmps5 &
 ./turnover.ce --snapshots 1 50 0.01 0.02 1.05 100 0.5 64 0.1 0.5 0.5 0.25 25 > tmps6 &
 
 
-./turnover.ce --simulate 250 50 0.01 0.02 1.05 100 0.1 > tmp1 &
-./turnover.ce --simulate 250 50 0.01 0.02 1.05 100 0.5 > tmp2 &
+./turnover.ce --simulate 500 50 0.01 0.02 1.05 100 0.1 4 > tmp1 &
+./turnover.ce --simulate 500 50 0.01 0.02 1.05 100 0.5 4 > tmp2 &
+./turnover.ce --simulate 500 50 0.01 0.02 1.05 100 0.1 16 > tmp3 &
+./turnover.ce --simulate 500 50 0.01 0.02 1.05 100 0.5 16 > tmp4 &
+./turnover.ce --simulate 500 50 0.01 0.02 1.05 100 0.1 64 > tmp5 &
+./turnover.ce --simulate 500 50 0.01 0.02 1.05 100 0.5 64 > tmp6 &
