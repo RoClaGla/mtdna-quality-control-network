a0df1bfb68ae0c69c2994f894b5739f99865f622
RoClaGla <95038302+RoClaGla@users.noreply.github.com>
2022-08-21 10:33:37 +0200

    Update analyse.sh

--- analyse.sh
+++ analyse.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+head -n1 output-4-1.050-100-0.1.csv > output-1.050-100.csv
+
+cat output-4-1.050-100-0.1.csv output-16-1.050-100-0.1.csv output-64-1.050-100-0.1.csv | grep -v "h" >> output-1.050-100.csv
+cat output-4-1.050-100-0.5.csv output-16-1.050-100-0.5.csv output-64-1.050-100-0.5.csv | grep -v "h" >> output-1.050-100.csv
+
+
 Rscript plotcell.R
 
-Rscript plot.R output2-100.csv 100
+Rscript plot.R output-1.050-100.csv 100
